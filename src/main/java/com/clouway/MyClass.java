7d11bcdbb395333c047d2aa50e824a7f57108721
Borislav Gadjev <gadjevb@gmail.com>
2016-07-05 17:02:11 +0300

    initial commit

---
+++ src/main/java/com/clouway/MyClass.java
@@ -0,0 +1,23 @@
+package com.clouway;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import java.util.Scanner;
+
+public class MyClass {
+
+    public static Logger log = Logger.getLogger(MyClass.class);
+
+    public static void main(String[] args){
+        BasicConfigurator.configure();
+        Sumator sumator = new Sumator();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter the numbers you want to sum:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int answer = sumator.sumInt(a,b);
+
+        log.info("Sumator was called with " + a + ", " + b + " and the answer is: " + answer);
+    }
+}
